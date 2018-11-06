3072f5d492e73d493b945ad03d21359308dc255d
Olivia Mudong <Oliviamudong@gmail.com>
2018-11-06 13:43:57 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/goFish.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goFish.dir/main.cpp.o"
+  "goFish.pdb"
+  "goFish"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goFish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
